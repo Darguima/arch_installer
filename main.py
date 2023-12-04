e149c068acdfca65284492b76a13e1cf784d0597
Darguima <dsgdevbraga@gmail.com>
2023-12-04 14:40:15 +0000

    New Python organization

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-import json
+from merge_json import merge_json
 from os import listdir
 from os.path import isfile, join
 
-def merge_dicts(*dicts):
-    result = {}
-    for d in dicts:
-        result.update(d)
-    return result
-
-def merge_json(environment):
-  config_base_file = open('config_base.json')
-  config_base = json.load(config_base_file)
-  config_base_file.close()
-
-  config_env_file = open(f'config_{environment}.json')
-  config_env = json.load(config_env_file)
-  config_env_file.close()
-
-  final_config = merge_dicts(config_base, config_env)
-  json_config = json.dumps(final_config, indent=4)
-
-  with open('user_configuration.json', 'w') as outfile:
-    outfile.write(json_config)
-
-if __name__ == '__main__':
+def get_env():
   print("\nAvailable environments: \n")
 
   config_files = []
@@ -37,6 +16,16 @@ if __name__ == '__main__':
 
   if not selected_env.isnumeric() or int(selected_env) > len(config_files):
     print("Invalid option!")
+    exit()
   else:
-    merge_json(config_files[int(selected_env) - 1])
-    print("\nConfiguration file generated successfully!")
+    return config_files[int(selected_env) - 1]
+
+def main():
+  env = get_env()
+  merge_json(env)
+  print("\nConfiguration file generated successfully! Now run:")
+  print("\n$ archinstall --config user_configuration.json")
+
+
+if __name__ == "__main__":
+  main()
---
+++ merge_json.py
@@ -0,0 +1,23 @@
+
+import json
+
+def merge_dicts(*dicts):
+    result = {}
+    for d in dicts:
+        result.update(d)
+    return result
+
+def merge_json(environment):
+  config_base_file = open('config_base.json')
+  config_base = json.load(config_base_file)
+  config_base_file.close()
+
+  config_env_file = open(f'config_{environment}.json')
+  config_env = json.load(config_env_file)
+  config_env_file.close()
+
+  final_config = merge_dicts(config_base, config_env)
+  json_config = json.dumps(final_config, indent=4)
+
+  with open('user_configuration.json', 'w') as outfile:
+    outfile.write(json_config)
